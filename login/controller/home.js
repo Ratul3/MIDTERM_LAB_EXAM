08468fbb4806eb0decf7cde7919c801c3fcb985a
Ratul Rayhan <ratulrayhan201@gmail.com>
2020-08-16 13:46:58 +0600

    Add files via upload

---
+++ login/controller/home.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var db = require.main.require('./models/db');
+var userModel = require.main.require('./models/user');
+var router = express.Router();
+
+
+router.get('*', function(req, res, next){
+	if(req.session.username == null){
+		res.redirect('/login');
+	}else{
+		next();
+	}
+});
+
+router.get('/', function(req, res){
+
+	
+		res.render('home/index', {uname: req.session.username});
+	
+});
+
+
+module.exports = router;
