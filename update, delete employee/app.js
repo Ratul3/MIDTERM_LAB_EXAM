98fbf3213f5822688483533128c43e787c9b58f7
Ratul Rayhan <ratulrayhan201@gmail.com>
2020-08-16 23:25:18 +0600

    Add files via upload

---
+++ update, delete employee/app.js
@@ -0,0 +1,36 @@
+var express 	= require('express');
+
+var exSession 	= require('express-session');
+var bodyParser 	= require('body-parser');
+var login 		= require('./controller/login');
+var registration = require('./controller/registration');
+var home 		= require('./controller/home');
+var employee 		= require('./controller/employee');
+var logout 		= require('./controller/logout');
+var app 		= express();
+
+//config
+app.set('view engine', 'ejs');
+
+
+
+
+//middleware
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(exSession({secret: 'my secret value', saveUninitialized: true, resave: false}));
+
+app.use('/registration', registration);
+app.use('/login', login);
+app.use('/logout', logout);
+app.use('/home', home);
+app.use('/employee', employee);
+
+
+app.get('/', function(req, res){
+	res.send("<fieldset><center><h1><font color='green'>Index Page</font></h1></center><center> <a href='/registration'> Registration</a> | <a href='/login'> Login</a> </center> </fieldset>");
+});
+
+app.listen(3000, function(){
+	console.log('express http server started at...3000');
+});
